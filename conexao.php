7f8105cb864fa3800a3a4203a624545555853336
Rafaelcostadelima <rafita.limagames2@gmail.com>
2026-08-06 11:48:01 -0300

    aula 02

--- conexao.php
+++ conexao.php
@@ -1,3 +1,15 @@
 <?php
+    $host = "localhost";
+    $usuario = "root";
+    $senha = "";
+    $banco = "sistema_produtos";
+    $conexao = mysqli_connect($host, $usuario, $senha, $banco);
 
+    // var_dump($conexao);
+
+    // if ($conexao) {
+    //     echo "Conexão realizada com sucesso! <br>";
+    // } else {
+    //     echo "Erro ao conectar: " . mysqli_connect_error();
+    // }
 ?>
