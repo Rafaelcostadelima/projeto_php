a7cf643e7b73579b11a13e026de39dcc7422cb1c
Rafaelcostadelima <rafita.limagames2@gmail.com>
2026-08-20 12:12:17 -0300

    Aula 4 - Administrador, sessões, etc.

--- login.php
+++ login.php
@@ -1,22 +1,25 @@
 <?php
-include 'cabecalho.php';
+session_start();
 include 'conexao.php';
-
+include 'cabecalho.php';
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $email = $_POST['email'];
     $senha = $_POST['senha'];
-
-
-    $sql = "SELECT * FROM usuarios WHERE email = '$email' AND senha = '$senha'";
+    $sql = "SELECT * FROM usuarios WHERE email = '$email' AND senha =
+'$senha'";
     $resultado = mysqli_query($conexao, $sql);
-
     if (mysqli_num_rows($resultado) == 1) {
-        $mensagem = "Login realizado com sucesso!";
+        $usuario = mysqli_fetch_assoc($resultado);
+        $_SESSION['usuario_id'] = $usuario['id'];
+        $_SESSION['usuario_nome'] = $usuario['nome'];
+        header("Location: produtos/listar.php");
+        exit;
     } else {
         $mensagem = "E-mail ou senha inválidos.";
     }
 }
 ?>
+
 
 <main>
     <h2>Login</h2>
--- produtos/excluir.php
+++ produtos/excluir.php
@@ -1,3 +1,5 @@
 <?php
-
+session_start();
+include 'conexao.php';
+include 'verifica_login.php';
 ?>
